79d87465536f455da8eedbd967399992b15e9677
Thuan Phan <thuanpx1710@gmail.com>
2019-03-26 15:26:54 +0700

    implement push notification on android

--- App.tsx
+++ App.tsx
@@ -6,56 +6,150 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from "react";
+import { Alert, Button, Platform, StyleSheet, Text, View } from "react-native";
+import { AsyncStorage } from "react-native";
+import firebase from "react-native-firebase";
 
 interface Props {
 }
 
 interface State {
-    name: String
+    name: string;
+    token: string;
 }
 
 export default class App extends Component<Props, State> {
-    constructor(props:Props){
-        super(props)
+    constructor(props: Props) {
+        super(props);
         this.state = {
-        name: 'Test Push Notification !',
-        }
+            name: "Test Push Notification !",
+            token: "",
+        };
     }
 
+    public async componentDidMount() {
+        this.checkPermission();
+        this.createNotificationListeners();
+    }
 
-    render() {
+    public componentWillUnmount() {
+        this.notificationListener();
+        this.notificationOpenedListener();
+    }
+
+    public render() {
+        console.log("ahihiD");
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}> {this.state.name}</Text>
+                <Text style={styles.welcome}> {this.state.token}</Text>
             </View>
         );
-  }
+    }
+
+    public async createNotificationListeners() {
+        /*
+        * Triggered when a particular notification has been received in foreground
+        * */
+        this.notificationListener();
+
+        /*
+        * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+        * */
+        this.notificationOpenedListener();
+
+        /*
+        * If your app is closed, you can check if it was opened
+        * by a notification being clicked / tapped / opened as follows:
+        * */
+        this.notificationOpen();
+        /*
+        * Triggered for data only payload in foreground
+        * */
+        this.messageListener();
+    }
+
+    public notificationListener() {
+        firebase.notifications().onNotification((notification) => {
+            const { title, body } = notification;
+            this.showAlert(title, "notificationListener");
+        });
+    }
+
+    public notificationOpenedListener() {
+        firebase.notifications().onNotificationOpened((notificationOpen) => {
+            const { title, body } = notificationOpen.notification;
+            this.showAlert(title, "notificationOpenedListener");
+        });
+    }
+
+    public messageListener() {
+        firebase.messaging().onMessage((message) => {
+            // process data message
+            console.log(JSON.stringify(message));
+            this.showAlert("title", "messageListener");
+        });
+    }
+
+    public async notificationOpen() {
+        const notificationOpen = await firebase.notifications().getInitialNotification();
+        if (notificationOpen) {
+            const { title, body } = notificationOpen.notification;
+            this.showAlert(title, "notificationOpen");
+        }
+    }
+
+    public showAlert(title: string, body: string) {
+        Alert.alert(
+            title, body,
+            [
+                { text: "OK", onPress: () => console.log("OK Pressed") },
+            ],
+            { cancelable: false },
+        );
+    }
+
+    public async checkPermission() {
+        const enable = await firebase.messaging().hasPermission();
+        if (enable) {
+            this.getToken();
+        } else {
+            this.requestPermission();
+        }
+    }
+
+    public async requestPermission() {
+        try {
+            await firebase.messaging().requestPermission();
+            this.getToken();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    public async getToken() {
+        let fcmToken = await AsyncStorage.getItem("fcmToken");
+        if (!fcmToken) {
+            fcmToken = await firebase.messaging().getToken();
+            if (fcmToken) {
+                await AsyncStorage.setItem("fcmToken", fcmToken);
+            }
+        }
+    }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF",
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10,
+    },
 });
--- android/app/src/main/java/com/rn_pushnotification/MainApplication.java
+++ android/app/src/main/java/com/rn_pushnotification/MainApplication.java
@@ -7,9 +7,14 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.BuildConfig;
 
 import java.util.Arrays;
 import java.util.List;
+
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNFirebasePackage(),
+          new RNFirebaseMessagingPackage(),
+          new RNFirebaseNotificationsPackage()
       );
     }
 
